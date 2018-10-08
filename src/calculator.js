b150cef96d73b58b61cf9de58e36c0c20286e1cf
egillthor96 <egillj17@ru.is>
2018-10-08 13:56:14 +0000

    Implemented for one number

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,9 @@
 function add(number) {
-	return 0;
+	if(number == "") {
+		return 0;
+	}
+	return parseInt(number);
+	
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,8 @@ const add = require("./calculator");
 it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 });
+
+
+it("should return number when only one number in string", () => {
+	expect(add("1")).toBe(1);
+});
